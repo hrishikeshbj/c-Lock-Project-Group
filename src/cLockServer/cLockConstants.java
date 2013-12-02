63be72393d0bbe4454c788e394ce29050fa2f198
hrishikeshbj <hrishikeshbj@ufl.edu>
2013-12-01 22:20:26 -0500

    Create cLockConstants.java

---
+++ src/cLockServer/cLockConstants.java
@@ -0,0 +1,21 @@
+package cLockServer;
+
+public class cLockConstants {
+
+	public static final String APPNAME = "c-Lock";
+	public static final String resourceTableName = "resourcetable";
+	
+	public static final String RID = "RID";
+	public static final String RNAME = "RNAME";
+	public static final String USER = "USER";
+	public static final String STATE = "STATE";
+	
+	public static final String OP_CREATE = "CREATE";
+	public static final String OP_DELETE = "DELETE";
+	public static final String OP_GET = "GET";
+	public static final String OP_CANCREATE = "CANCREATE";
+	public static final String OP_LOCK = "LOCK";
+	public static final String OP_RELEASE = "RELEASE";
+	public static final String OP_SUCCESS = "SUCCESS";
+	public static final String OP_FAIL = "FAIL";
+}
