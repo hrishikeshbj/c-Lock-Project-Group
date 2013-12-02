82caeb8b66c20acf3171d681a60afc6cd5f06673
hrishikeshbj <hrishikeshbj@ufl.edu>
2013-12-01 22:25:58 -0500

    Update LockType.java

--- src/cLockServer/LockType.java
+++ src/cLockServer/LockType.java
@@ -1,19 +1,28 @@
-package clockserver;
+package cLockServer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class LockRequest {
-
-	String user;
-	List<String> rList;
+public enum LockType {
+	NL, 		// Null lock 
+	CR,			// Concurrent Read Lock 
+	CW,			// Concurrent Write Lock 
+	PR,			// Protected Read Lock 
+	PW,			// Protected Write Lock 
+	XL; 		// Exclusive Lock
 	
-	public LockRequest(String user) {
-		rList = new ArrayList<String>();
-		this.user = user;
-	}
-	
-	public void putRequest(String rId) {
-		this.rList.add(rId);
+	public static LockType getLType(String type) {
+		
+		if(type.equalsIgnoreCase("NL")) {
+			return NL;
+		} else if(type.equalsIgnoreCase("CR")) {
+			return CR;
+		} else if(type.equalsIgnoreCase("CW")) {
+			return CW;
+		} else if(type.equalsIgnoreCase("PR")) {
+			return PR;
+		} else if(type.equalsIgnoreCase("PW")) {
+			return PW;
+		} else if(type.equalsIgnoreCase("XL")) {
+			return XL;
+		}
+		return NL;
 	}
 }
