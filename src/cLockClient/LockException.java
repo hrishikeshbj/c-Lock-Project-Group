df044258f671a01a19de1f910dadcaa8ffa1b450
nachiketpatil <hi.nachiket.patil@gmail.com>
2013-12-05 02:28:43 -0500

    Update LockException.java

--- src/cLockClient/LockException.java
+++ src/cLockClient/LockException.java
@@ -1,5 +1,11 @@
 package cLockClient;
 
+/**
+ * Exception to be thrown by the cLock client when an error occurs.
+ * 
+ * @author Nachiket
+ * 
+ */
 public class LockException extends Exception {
 
 	public LockException(String string) {
