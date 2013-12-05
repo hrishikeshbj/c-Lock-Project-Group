72e638d2f13709c72324e95f36a4e8e8b3e9ebee
nachiketpatil <hi.nachiket.patil@gmail.com>
2013-12-05 00:40:05 -0500

    Update cLockError.java

--- src/cLockServer/cLockError.java
+++ src/cLockServer/cLockError.java
@@ -1,10 +1,17 @@
-package clockserver;
+package cLockServer;
 
+/**
+ * Enumeration class to specify an error. These errors are passed as parameters
+ * to operation performed and to used if severe.
+ * 
+ * @author Nachiket
+ * 
+ */
 public enum cLockError {
-	NE, 		// No Error.
-	UE,			// Unknown Error.
-	IR,			// Invalid Resource Id.
-	IS,			// Invalid State.
-	LR;			// Lock revoked or lock not present.
-	
+	NE, // No Error.
+	UE, // Unknown Error.
+	IR, // Invalid Resource Id.
+	IS, // Invalid State.
+	LR, // Lock revoked or lock not present.
+	DL; // Deadlock present.
 }
